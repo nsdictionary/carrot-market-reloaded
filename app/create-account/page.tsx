781ac82a21dc31cdc303a5658729ff8f8e1721f9
sam <3161728@gmail.com>
2024-03-28 11:35:46 +0900

    #4.3 Log in Screen

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,6 +1,6 @@
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
-import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
+import SocialLogin from "@/components/social-login";
 import Link from "next/link";
 
 export default function CreateAccount() {
@@ -8,7 +8,7 @@ export default function CreateAccount() {
     <div className="flex flex-col gap-10 px-8 py-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Fill in the form below th join!</h2>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form className="flex flex-col gap-3">
         <FormInput type="text" placeholder="Username" required errors={[]} />
@@ -27,18 +27,7 @@ export default function CreateAccount() {
         />
         <FormButton loading={false} text="Create account" />
       </form>
-      <div className="w-full h-px bg-neutral-500" />
-      <div>
-        <Link
-          className="primary-btn flex h-10 items-center justify-center gap-3"
-          href="sms"
-        >
-          <span>
-            <ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" />
-          </span>
-          <span>Sign up with SMS</span>
-        </Link>
-      </div>
+      <SocialLogin />
     </div>
   );
 }
---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
+import Link from "next/link";
+
+export default function LogIn() {
+  return (
+    <div className="flex flex-col gap-10 px-8 py-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Log in" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+}
