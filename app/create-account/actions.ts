d744bcd8f9e9f270b94c1ef90a75d43e827272ff
sam <3161728@gmail.com>
2024-04-02 17:03:52 +0900

    #8.2 Password Hashing

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
@@ -6,6 +7,7 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -83,8 +85,21 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    // hash password
-    // save the user to db
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    console.log(user);
+
     // log the user in
     // redirect "/home"
   }
