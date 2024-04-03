89172b7dfdbe6a14f53706d8e678a0c8818d329c
sam <3161728@gmail.com>
2024-04-03 15:26:38 +0900

    #8.11 Edge Runtime

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,7 @@
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  if (pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookie", "hello");
-    return response;
-  }
-
-  if (pathname == "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-}
+export async function middleware(request: NextRequest) {}
 
 export const config = {
-  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  matcher: ["/", "profile", "create-account", "/user/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
