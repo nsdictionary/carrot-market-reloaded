6d4aa6e292ec59ca77895fd4bcd65fa809f40ff8
sam <3161728@gmail.com>
2024-04-03 15:22:07 +0900

    #8.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,21 @@
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname == "/profile") {
-    return Response.redirect(new URL("/", request.url));
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello");
+    return response;
+  }
+
+  if (pathname == "/profile") {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/", "profile", "create-account", "/user/:path*"],
+};
