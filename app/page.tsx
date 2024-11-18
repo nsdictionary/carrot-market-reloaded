99f6634bd725c332752dae0b0698f1915f102969
sam <3161728@gmail.com>
2024-11-18 11:02:48 +0900

    a23

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,53 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="text-4xl font-semibold dark:text-white">
-              Coolblue
-            </span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:bg-green-500 hover:scale-125">
-            Today
+    <main className="bg-yellow-300 p-8 pt-64 rounded-[32px] max-w-sm">
+      <h2 className="text-black font-bold text-xl mb-4 border-b border-black pb-2">
+        Select Destination
+      </h2>
+      <ul className="space-y-1">
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
+            v
           </span>
-          <span className="dark:text-gray-100">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 absolute rounded-full w-full h-2" />
-          <div className="bg-green-400 absolute rounded-full w-2/3 h-2" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300 ">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
-      </div>
+          <span className="text-black text-2xl font-bold">Vancouver</span>
+        </li>
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
+            s
+          </span>
+          <span className="text-black text-2xl font-bold">Seattle</span>
+        </li>
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
+            p
+          </span>
+          <span className="text-black text-2xl font-bold">Portland</span>
+        </li>
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
+            s
+          </span>
+          <span className="text-black text-2xl font-bold">Seattle</span>
+        </li>
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-white text-yellow-200 rounded-full flex items-center justify-center text-base font-semibold">
+            s
+          </span>
+          <span className="text-white text-2xl font-bold">San Francisco</span>
+        </li>
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
+            l
+          </span>
+          <span className="text-black text-2xl font-bold">Los Angeles</span>
+        </li>
+        <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
+          <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
+            s
+          </span>
+          <span className="text-black text-2xl font-bold">San Diego</span>
+        </li>
+      </ul>
     </main>
   );
 }
