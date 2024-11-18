003e965782cd6d6e2d80ed53075fb91af8a9c356
sam <3161728@gmail.com>
2024-11-18 14:22:08 +0900

    #3.5 Responsive Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,16 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <main className="bg-yellow-300 p-8 pt-64 rounded-[32px] max-w-sm shadow-2xl">
-        <h2 className="text-black font-bold text-xl mb-4 border-b border-black pb-2">
-          Select Destination
-        </h2>
-        <ul className="space-y-1">
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
-              v
-            </span>
-            <span className="text-black text-2xl font-bold">Vancouver</span>
-          </li>
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
-              s
-            </span>
-            <span className="text-black text-2xl font-bold">Seattle</span>
-          </li>
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
-              p
-            </span>
-            <span className="text-black text-2xl font-bold">Portland</span>
-          </li>
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
-              s
-            </span>
-            <span className="text-black text-2xl font-bold">Seattle</span>
-          </li>
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-white text-yellow-200 rounded-full flex items-center justify-center text-base font-semibold">
-              s
-            </span>
-            <span className="text-white text-2xl font-bold">San Francisco</span>
-          </li>
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
-              l
-            </span>
-            <span className="text-black text-2xl font-bold">Los Angeles</span>
-          </li>
-          <li className="flex items-center space-x-4 hover:bg-yellow-200 p-2 rounded-lg cursor-pointer">
-            <span className="w-7 h-7 bg-black text-yellow-300 rounded-full flex items-center justify-center text-base font-semibold">
-              s
-            </span>
-            <span className="text-black text-2xl font-bold">San Diego</span>
-          </li>
-        </ul>
-      </main>
-    </div>
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+        <input
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button className="bg-black text-white py-2 rounded-full active:scale-90  transition-transform font-medium outline-none md:px-10 ">
+          Search
+        </button>
+      </div>
+    </main>
   );
 }
