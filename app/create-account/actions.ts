60b819457d94b2b4fd2c5dc2525f9f53d42d034d
sam <3161728@gmail.com>
2024-11-24 16:35:14 +0900

    #6.4 Refactor

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -13,7 +13,7 @@ const formSchema = z
         required_error: "Where is my username???",
       })
       .min(3, "Way too short!!!")
-      //.max(10, "That is too looooong!")
+      .max(10, "That is too looooong!")
       .trim()
       .toLowerCase()
       .transform((username) => `🔥 ${username}`)
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,5 +1,5 @@
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+import Button from "@/components/button";
+import Input from "@/components/input";
 export default function SMSLogin() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -8,19 +8,14 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput
-          type="number"
-          placeholder="Phone number"
-          required
-          errors={[]}
-        />
-        <FormInput
+        <Input name="phone" type="number" placeholder="Phone number" required />
+        <Input
+          name="code"
           type="number"
           placeholder="Verification code"
           required
-          errors={[]}
         />
-        <FormButton loading={false} text="Verify" />
+        <Button text="Verify" />
       </form>
     </div>
   );
