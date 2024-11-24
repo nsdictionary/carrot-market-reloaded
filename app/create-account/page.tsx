93b13f72806ac13ccc9147a88edc2466cd7a1d17
sam <3161728@gmail.com>
2024-11-24 14:26:42 +0900

    #6.1 Validation Errors

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -3,11 +3,11 @@
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
 import { createAccount } from "./actions";
-import { useActionState } from "react";
 
 export default function CreateAccount() {
-  const [state, dispatch] = useActionState(createAccount, null);
+  const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -20,19 +20,28 @@ export default function CreateAccount() {
           type="text"
           placeholder="Username"
           required
+          errors={state?.fieldErrors.username}
         />
-        <FormInput name="email" type="email" placeholder="Email" required />
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
+          errors={state?.fieldErrors.password}
         />
         <FormInput
           name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
+          errors={state?.fieldErrors.confirm_password}
         />
         <FormButton text="Create account" />
       </form>
