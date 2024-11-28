8dc673f293dd596633704972663f8db830715aeb
sam <3161728@gmail.com>
2024-11-28 14:22:52 +0900

    #8.7 Log Out

--- lib/session.ts
+++ lib/session.ts
@@ -1,8 +1,10 @@
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
+
 interface SessionContent {
   id?: number;
 }
+
 export default async function getSession() {
   return getIronSession<SessionContent>(await cookies(), {
     cookieName: "delicious-karrot",
